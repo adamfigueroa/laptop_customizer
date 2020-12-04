c01e2fd10506cf9205d67eb71eb5c6df2d41a860
Adam Figueroa <admiralfiggins@gmail.com>
2020-12-04 14:50:16 -0800

    finished feature now onto cart

--- src/Cart.js
+++ src/Cart.js
@@ -1,2 +1,19 @@
 import React from "react"
 
+function Cart(props) {
+
+    const summary = Object.keys(this.state.selected).map((feature, idx) => {
+        const featureHash = feature + '-' + idx;
+        const selectedOption = this.state.selected[feature];
+  
+        return (
+          <div className="summary__option" key={featureHash}>
+            <div className="summary__option__label">{feature} </div>
+            <div className="summary__option__value">{selectedOption.name}</div>
+            <div className="summary__option__cost">
+              {props.USCurrencyFormat.format(selectedOption.cost)}
+            </div>
+          </div>
+        );
+      });
+}
--- src/Form/SelectionForm.js
+++ src/Form/SelectionForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import slugify from 'slugify';
+import Feature from '../Feature/Feature'
+
 
 const FEATURES = {
     Processor: [
@@ -50,32 +51,16 @@ function SelectionForm(props) {
 
     const features = Object.keys(FEATURES).map((feature, idx) => {
         const featureHash = feature + '-' + idx;
-        const options = FEATURES[feature].map(item => {
-          const itemHash = slugify(JSON.stringify(item));
-          return (
-            <div key={itemHash} className="feature__item">
-              <input
-                type="radio"
-                id={itemHash}
-                className="feature__option"
-                name={slugify(feature)}
-                checked={item.name === props.selected[feature].name}
-                onChange={e => props.updateFeature(feature, item)}
-              />
-              <label htmlFor={itemHash} className="feature__label">
-                {item.name} ({props.USCurrencyFormat.format(item.cost)})
-              </label>
-            </div>
-          );
-        });
-  
+        // return a feature component on each loop
         return (
-          <fieldset className="feature" key={featureHash}>
-            <legend className="feature__name">
-              <h3>{feature}</h3>
-            </legend>
-            {options}
-          </fieldset>
+         <Feature 
+         FEATURES={FEATURES} 
+         feature={feature} 
+         selected={props.selected}
+         key={featureHash}
+         updateFeature={props.updateFeature}
+         USCurrencyFormat={props.USCurrencyFormat}
+         />
         );
       });
 
